325e85ee1594159d4785a07667e44ef46bb986a8
Radilo <radilo86@gmail.com>
2022-01-25 17:52:55 +0100

    v1.3

--- src/main/java/radilo86/Spring_Ejercicio4_5_6/Ejercicio3/LaptopV2Controller.java
+++ src/main/java/radilo86/Spring_Ejercicio4_5_6/Ejercicio3/LaptopV2Controller.java
@@ -1,10 +1,11 @@
 package radilo86.Spring_Ejercicio4_5_6.Ejercicio3;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import radilo86.Spring_Ejercicio4_5_6.Ejercicio2.Entidad.Laptop;
 import radilo86.Spring_Ejercicio4_5_6.Ejercicio2.Repository.LaptopRepository;
+
+import java.util.Optional;
 
 @RestController
 public class LaptopV2Controller {
@@ -32,5 +33,23 @@ public class LaptopV2Controller {
     }
 
     // Busca un registro segun su id
-
+    @GetMapping("/api/byId/{id}") // Entre los corchetes pasamos el parametro a buscar
+    public ResponseEntity<Laptop> encontrarPorID(@PathVariable Long id){
+        /*
+        * ResponseEntity maneja toda la respuesta HTTP incluyendo el cuerpo, cabecera y códigos de estado
+        * permitiéndonos total libertad de configurar la respuesta que queremos que se envié desde nuestros endpoints.
+        * Optional<Clase> es un envoltorio, que es necesario puesto que el crud nos devuelve este tipo de dato.
+        * una vez declarado e instanciado podemos comprobar si el dato existe o no mediante el metodo .isPresent()
+        * y devolveremos un ResponseEntity.ok o .notFound(), en funcion de si existe o no esa id.
+        */
+        Optional<Laptop> laptopOpt = laptopRepository.findById(id);
+        if (laptopOpt.isPresent()){
+            // Devolvemos el ResponseEntity con el estado OK, junto con los valores que contiene esa Id
+            return ResponseEntity.ok(laptopOpt.get());
+        }
+        else{
+            // Devolvemos ResponseEntity con el estado no encontrado (404)
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
